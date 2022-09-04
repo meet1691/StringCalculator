53231d03cec406ad11e4e78e9b44192ed70af141
Meet <patelmeet1691@gmail.com>
2022-09-04 19:56:01 +0530

    7th test case passed

--- src/Test/StringCalculatorTest.java
+++ src/Test/StringCalculatorTest.java
@@ -1,59 +1,74 @@
 package Test;
+
 import com.StringCalculator;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 public class StringCalculatorTest {
     StringCalculator stringCalculator;
+
     @Before
-    public void setUp(){
-         stringCalculator = new StringCalculator();
+    public void setUp() {
+        stringCalculator = new StringCalculator();
     }
-    @Test public void blankStringTest(){
+
+    @Test
+    public void blankStringTest() {
         //check for blank string
-        assertEquals(0,stringCalculator.add(""));
+        assertEquals(0, stringCalculator.add(""));
     }
-    @Test public void oneOrTwoNumberTest(){
-        assertEquals(1,stringCalculator.add("1"));
-        assertEquals(3,stringCalculator.add("1,2"));
-        assertEquals(80,stringCalculator.add("30,50"));
+
+    @Test
+    public void oneOrTwoNumberTest() {
+        assertEquals(1, stringCalculator.add("1"));
+        assertEquals(3, stringCalculator.add("1,2"));
+        assertEquals(80, stringCalculator.add("30,50"));
     }
-    @Test public void unknownAmountOfNumberTest(){
-        assertEquals(7,stringCalculator.add("5,2"));
-        assertEquals(6,stringCalculator.add("1,2,3"));
-        assertEquals(55,stringCalculator.add("1,2,3,4,5,6,7,8,9,10"));
+
+    @Test
+    public void unknownAmountOfNumberTest() {
+        assertEquals(7, stringCalculator.add("5,2"));
+        assertEquals(6, stringCalculator.add("1,2,3"));
+        assertEquals(55, stringCalculator.add("1,2,3,4,5,6,7,8,9,10"));
     }
-    @Test public void numberAndAlphabetTest(){
-        assertEquals(4,stringCalculator.add("1,a,b"));
-        assertEquals(7,stringCalculator.add("1,2,a,c"));
-        assertEquals(44,stringCalculator.add("1,a,3,5,6,b,z"));
-        assertEquals(50,stringCalculator.add("a,3,5,b,z,3,j"));
+
+    @Test
+    public void numberAndAlphabetTest() {
+        assertEquals(4, stringCalculator.add("1,a,b"));
+        assertEquals(7, stringCalculator.add("1,2,a,c"));
+        assertEquals(44, stringCalculator.add("1,a,3,5,6,b,z"));
+        assertEquals(50, stringCalculator.add("a,3,5,b,z,3,j"));
     }
 
 
-    @Test public void ThrowNegativeNotAllowedExceptionTest(){
-        assertThrows("Negatives Not allowed",IllegalArgumentException.class,()->stringCalculator.add("1,-2"));
-        assertThrows("Negatives Not allowed",IllegalArgumentException.class,()->stringCalculator.add("1,3,4,5,-2"));
+    @Test
+    public void ThrowNegativeNotAllowedExceptionTest() {
+        assertThrows("Negatives Not allowed", IllegalArgumentException.class, () -> stringCalculator.add("1,-2"));
+        assertThrows("Negatives Not allowed", IllegalArgumentException.class, () -> stringCalculator.add("1,3,4,5,-2"));
     }
 
 
-    @Test public void showNegativesTest(){
-        assertThrows(IllegalArgumentException.class,()->stringCalculator.add("-2,6"));
-        assertThrows(IllegalArgumentException.class,()->stringCalculator.add("-2,-3,4,-5,6"));
-        assertThrows(IllegalArgumentException.class,()->stringCalculator.add("-2,-3,4,-5,6,-8,6,3"));
+    @Test
+    public void showNegativesTest() {
+        assertThrows(IllegalArgumentException.class, () -> stringCalculator.add("-2,6"));
+        assertThrows(IllegalArgumentException.class, () -> stringCalculator.add("-2,-3,4,-5,6"));
+        assertThrows(IllegalArgumentException.class, () -> stringCalculator.add("-2,-3,4,-5,6,-8,6,3"));
     }
 
-    @Test public void numberLargerThan1000ShouldIgnoreTest(){
-        assertEquals(2,stringCalculator.add("2,1001"));
-        assertEquals(64,stringCalculator.add("10,4,50,6000,4444"));
+    @Test
+    public void numberLargerThan1000ShouldIgnoreTest() {
+        assertEquals(2, stringCalculator.add("2,1001"));
+        assertEquals(64, stringCalculator.add("10,4,50,6000,4444"));
     }
 
+    @Test
+    public void allowIfStringContainNewLineTest() {
+        assertEquals(6, stringCalculator.add("1\n2,3"));
+        assertEquals(27, stringCalculator.add("1\n2,3,5,6,9\n1"));
+    }
 
 
 }
--- src/com/StringCalculator.java
+++ src/com/StringCalculator.java
@@ -2,11 +2,11 @@ package com;
 
 import java.util.ArrayList;
 
-public class StringCalculator extends Exception{
+public class StringCalculator extends Exception {
 
-    public int add(String numbers){
+    public int add(String numbers) {
 
-        int sum = 0 ;
+        int sum = 0;
 
         if (numbers.isEmpty())
             return 0;
@@ -15,29 +15,39 @@ public class StringCalculator extends Exception{
             String[] elements = numbers.split(",");
             ArrayList<String> listOfNegative = new ArrayList<>();
 
-            for (String element: elements) {
-                if (element.contains("-")){
+            for (String element : elements) {
+                if (element.contains("-")) {
                     listOfNegative.add(element);
                     continue;
                 }
                 // here I assume 1000 should be allowed to add . If not then code should be "if (element.length()>3){...}"
-                if (!element.equals("1000") && element.length()>3){
+                if (!element.equals("1000") && element.length() > 3) {
+                    continue;
+                }
+                if (element.contains("\n")) {
+                    char[] charWithSpace = element.toCharArray();
+                    for (char digit : charWithSpace) {
+                        if (Character.isDigit(digit)) {
+                            int numberInString = Integer.parseInt(Character.toString(digit));
+                            sum += numberInString;
+                        }
+
+                    }
                     continue;
                 }
                 try {
                     int num = Integer.parseInt(element);
-                    sum+=num;
+                    sum += num;
                 } catch (NumberFormatException nfe) {
                     char current = element.charAt(0);
-                    int index = current -'a'+1 ;
-                    sum+=index;
+                    int index = current - 'a' + 1;
+                    sum += index;
                 }
             }
 
-            if (listOfNegative.size()>0){
-                throw new IllegalArgumentException("Negative Numbers Are :"+listOfNegative);
+            if (listOfNegative.size() > 0) {
+                throw new IllegalArgumentException("Negative Numbers Are :" + listOfNegative);
             }
-
 
         }
 
